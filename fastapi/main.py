ee7678c959a09feb9cc4b37d145270cb1275867a
ashwanth <ashwantheee07@gmail.com>
2024-04-03 16:38:14 +0530

    changes1

--- fastapi/main.py
+++ fastapi/main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/")
 def index():
     return{"Msg":"Api working sucessfully"}
+
+@app.get("/about")
+def about():
+    return{"Mgs":"About Page"}
+
+@app.get("/user/{id}")
+def user(id:int):
+    return{"data":{"id":id}}
